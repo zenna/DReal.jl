6f6b92e32148a209b09aeb5470938298ad85c51f
Eric Davies <iamed2@gmail.com>
2015-07-24 00:39:22 -0500

    Removed 0.4 enum (wasn't working, 0.3 version is fine)

--- src/logic.jl
+++ src/logic.jl
@@ -1,13 +1,19 @@
-@enum(Logic,
-  qf_uf,         # Uninterpreted Functions
-  qf_nra,        # Non-Linear Real Arithmetic
-  qf_nra_ode,    # Non-Linear Real Arithmetic
-  qf_bv,         # BitVectors
-  qf_rdl,        # Real difference logics
-  qf_idl,        # Integer difference logics
-  qf_lra,        # Real linear arithmetic
-  qf_lia,        # Integer linear arithmetic
-  qf_ufidl,      # UF + IDL
-  qf_uflra,      # UF + LRA
-  qf_bool,       # Only booleans
-  qf_ct)         # Cost
+## Compatability for julia0.3
+## ==========================
+
+immutable Logic
+  val::Int8
+end
+
+ const qf_uf = Logic(0)         # Uninterpreted Functions
+ const qf_nra = Logic(1)        # Non-Linear Real Arithmetic
+ const qf_nra_ode = Logic(2)    # Non-Linear Real Arithmetic
+ const qf_bv = Logic(3)         # BitVectors
+ const qf_rdl = Logic(4)        # Real difference logics
+ const qf_idl = Logic(5)        # Integer difference logics
+ const qf_lra = Logic(6)        # Real linear arithmetic
+ const qf_lia = Logic(7)        # Integer linear arithmetic
+ const qf_ufidl = Logic(8)      # UF + IDL
+ const qf_uflra = Logic(9)      # UF + LRA
+ const qf_bool = Logic(10)      # Only booleans
+ const qf_ct = Logic(11)        # Costqf
