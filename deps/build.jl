1d46446b202dc8d976f57c687d249a0a117605ed
Zenna Tavares <zennatavares@gmail.com>
2015-07-24 11:53:37 -0400

    osx fix

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,9 @@
 version = "3.15.07.02"
 os_string = @osx? "darwin" : "linux"
-file_url = "https://github.com/dreal/dreal3/releases/download/v$version/dReal-$version-$os_string-shared-libs.tar.gz"
+extension = @osx? "zip" : "tar.gz"
+
+file_name = "dReal-$version-$os_string-shared-libs.$extension"
+file_url = "https://github.com/dreal/dreal3/releases/download/v$version/$file_name"
 deps_dir = joinpath(joinpath(Pkg.dir("DReal"),"deps"))
 prefix = joinpath(deps_dir,"usr")
 
@@ -14,10 +17,10 @@ try
   run(`rm -r $(joinpath(deps_dir,"usr"))`)
 catch end
 
-download(file_url,joinpath(deps_dir,"dReal-$version-$os_string-shared-libs.tar.gz"))
+download(file_url,joinpath(deps_dir,file_name))
 @osx? begin
-  run(`unzip dReal-$version-$os_string-shared-libs.zip`)
+  run(`unzip $file_name`)
 end : begin
-  run(`tar -xvf dReal-$version-$os_string-shared-libs.tar.gz`)
+  run(`tar -xvf $file_name`)
 end
 run(`mv $(joinpath(deps_dir, "dReal-$version-$os_string-shared-libs")) usr`)
